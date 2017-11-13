f6b2cd7ac5f84c96153e9b8ec44d6630269d2c1c
deathedge2013 <deathedge2013@gmail.com>
2017-11-13 12:04:58 +0800

    add page

--- sql.php
+++ sql.php
@@ -2,7 +2,7 @@
 
 // Connect to the db
 function ConnectDB(){
-    $mysqli = new mysqli("localhost", "root", "", "book_vehicle") or die("Cannot connect to database server! Please check the connection.");
+    $mysqli = new mysqli("assignment.cprfoxekccb6.ap-southeast-1.rds.amazonaws.com", "admin", "password", "book_vehicle") or die("Cannot connect to database server! Please check the connection.");
     return $mysqli;
 }
 
