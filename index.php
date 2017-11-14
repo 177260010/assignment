01046e9cb57da3ca35422f854aa3257bbd2c3b1b
deathedge2013 <deathedge2013@gmail.com>
2017-11-14 16:42:00 +0800

    change html file to php file

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: KaYu
- * Date: 14/11/2017
- * Time: 14:46
- */
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Vehicle Booking Service</title>
+</head>
+<body>
+<h1>Vehicle Booking</h1>
+<table>
+    <tr>
+        <td><a href="login.php">Login</a></td>
+    </tr>
+</table>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: KaYu
- * Date: 14/11/2017
- * Time: 14:45
- */
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+</head>
+<body>
+<form method="post" action="login_process.php">
+    <div class="container">
+        <table>
+            <tr>
+                <td><label>Username: </label></td>
+                <td><input type="text" placeholder="Username" name="username" required></td>
+            </tr>
+            <tr>
+                <td><label>Password: </label></td>
+                <td><input type="password" placeholder="Password" name="password" required></td>
+            </tr>
+        </table>
+        <input type="checkbox" value="checked">Remind Me
+        <input type="submit" value="Login">
+    </div>
+</form>
+<a href="register.html">Register</a>
+</body>
+</html>
+
