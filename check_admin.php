032e5749e92196b878b6a7bbdff426e160adc1f6
deathedge <a@a>
2017-11-17 15:44:22 +0800

    'aa'

--- check_admin.php
+++ check_admin.php
@@ -3,11 +3,11 @@ if ($login === 'admin') {
 
 } elseif ($login <> "admin") {
     echo "You are not admin!!!<p>";
-    echo '<a href="index.php">Login</a>';
+    echo '<a href="index.php">Home</a>';
     exit();
 } else {
     echo "You are not admin!!!<p>";
-    echo '<a href="index.php">Login</a>';
+    echo '<a href="index.php">Home</a>';
     exit();
 }
 
