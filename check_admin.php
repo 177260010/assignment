2ee968815ba32404fac694a13c00fe2c2d05432a
deathedge <a@a>
2017-11-17 15:30:56 +0800

    'aa'

--- check_admin.php
+++ check_admin.php
@@ -3,12 +3,12 @@
 if (isset($_SESSION['login'])) {
     if (isset($_SESSION['login']) <> "admin") {
         echo "You are not admin!!!<p>";
-        echo '<a href="login.html">Login</a>';
+        echo '<a href="index.php">Login</a>';
         exit();
     }
 } else {
     echo "You are not admin!!!<p>";
-    echo '<a href="login.html">Login</a>';
+    echo '<a href="index.php">Login</a>';
     exit();
 }
 
